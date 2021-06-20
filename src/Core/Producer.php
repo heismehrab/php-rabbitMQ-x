a0999960a8f553c2b98bb603bf5352560f79397a
heismehrab <hedayatmanesh.mehrab@gmail.com>
2021-06-20 22:47:39 +0430

    modify producer class according to base handler class modification

--- src/Core/Producer.php
+++ src/Core/Producer.php
@@ -2,12 +2,8 @@
 
 namespace HeIsMehrab\PhpRabbitMq\Core;
 
-use Exception, InvalidArgumentException;
-
-use PhpAmqpLib\Channel\AMQPChannel;
+use Exception;
 use PhpAmqpLib\Message\AMQPMessage;
-
-use HeIsMehrab\PhpRabbitMq\Config\Config;
 use HeIsMehrab\PhpRabbitMq\Services\RabbitMQ\RabbitMQService;
 
 /**
@@ -17,42 +13,8 @@ use HeIsMehrab\PhpRabbitMq\Services\RabbitMQ\RabbitMQService;
  *
  * @package HeIsMehrab\PhpRabbitMq\Core
  */
-class Producer
+class Producer extends BaseHandler
 {
-    /**
-     * Keep instance of RabbitMQ.
-     *
-     * @var RabbitMQService|AMQPChannel $node
-     */
-    private $node;
-
-    /**
-     * Keep the message/task.
-     *
-     * @var string $message
-     */
-    public $message;
-
-    /**
-     * Keep exchanges.
-     *
-     * @var array $exchanges
-     */
-    public $exchanges = Config::EXCHANGES;
-
-    /**
-     * Keep exchange type.
-     *
-     * @var string $defaultExchange
-     */
-    public $defaultExchange = Config::DEFAULT_EXCHANGE;
-
-    /**
-     * Keep queues.
-     *
-     * @var string $queues
-     */
-    public $queues = Config::QUEUES;
 
     /**
      * Producer constructor.
@@ -60,54 +22,6 @@ class Producer
     public function __construct()
     {
         $this->node = RabbitMQService::node();
-    }
-
-    /**
-     * Set the message.
-     *
-     * @param string $message
-     *
-     * @return void
-     */
-    public function setMessage(string $message)
-    {
-        $this->message = $message;
-    }
-
-    /**
-     * Set the exchanges.
-     *
-     * @param array $exchanges
-     *
-     * @return void
-     */
-    public function setExchanges(array $exchanges)
-    {
-       $this->exchanges = $exchanges;
-    }
-
-    /**
-     * Set the default exchange.
-     *
-     * @param string $name
-     *
-     * @return void
-     */
-    public function setDefaultExchange(string $name)
-    {
-        $this->defaultExchange = $name;
-    }
-
-    /**
-     * Set the queues.
-     *
-     * @param string $queues
-     *
-     * @return void
-     */
-    public function setQueues(string $queues)
-    {
-        $this->queues = $queues;
     }
 
     /**
@@ -140,58 +54,5 @@ class Producer
 
         // Remove un-used data.
         unset($message);
-    }
-
-    /**
-     * Declare defined queues.
-     *
-     * @throws Exception
-     *
-     * @return void
-     */
-    private function declareQueues()
-    {
-        if (! is_array($this->queues)) {
-            $errorMessage = 'argument queues must be array, not %s';
-
-            throw new InvalidArgumentException(sprintf($errorMessage, gettype($this->queues)));
-        }
-
-        if (! count($this->queues)) {
-            $errorMessage = 'argument queues can not be empty or Null';
-
-            throw new Exception($errorMessage);
-        }
-
-        foreach ($this->queues as $queue) {
-            $this->node->queue_declare(
-                $queue,
-                false,
-                true,
-                false,
-                false
-            );
-        }
-    }
-
-    /**
-     * Declare defined exchanges.
-     *
-     * @return void
-     */
-    private function declareExchanges() : void
-    {
-        if (count($this->exchanges)) {
-
-            foreach ($this->exchanges as $name => $data) {
-                $this->node->exchange_declare(
-                    $name,
-                    $data['type'],
-                    false,
-                    false,
-                    false
-                );
-            }
-        }
     }
 }
