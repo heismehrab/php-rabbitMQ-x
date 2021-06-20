36e1fbf3a617add01706a0b8cfeee056ec0ae46a
heismehrab <hedayatmanesh.mehrab@gmail.com>
2021-06-20 20:38:02 +0430

    update exchange details

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -12,12 +12,49 @@ namespace HeIsMehrab\PhpRabbitMq\Config;
 class Config
 {
     /**
+     * Keep an exchange type.
+     *
+     * @var string EXCHANGE_TYPE_FANOUT
+     */
+    private const EXCHANGE_TYPE_FANOUT = 'fanout';
+
+    /**
+     * Keep an exchange type.
+     *
+     * @var string EXCHANGE_TYPE_DIRECT
+     */
+    private const EXCHANGE_TYPE_DIRECT = 'direct';
+
+    /**
+     * Keep an array exchange types (only supported in this package).
+     *
+     * @var array EXCHANGE_TYPES
+     */
+    public const EXCHANGE_TYPES = [
+        null,
+        self::EXCHANGE_TYPE_FANOUT,
+        self::EXCHANGE_TYPE_DIRECT
+    ];
+
+    /**
      * Define the default type of your exchange,
-     * for now you can use null, fanout, dire
+     * for now you can use null, fanout and direct.
+     *
+     * Null: for RabbitMQ instance without any exchanges.
+     *
+     * Fanout: for RabbitMQ instance with fanout exchange,
+     * witch use the values of array EXCHANGES (see the constants below),
+     * if you define more than one index in that array, system will
+     * merge the all its values together.
+     *
+     * Direct: for RabbitMQ instance with direct exchange,
+     * witch use the EXCHANGES array (see the constants below),
+     * assume its indexes as a exchange names and its values
+     * as queue names.
      *
      * @var string EXCHANGE_TYPE
      */
-    public const EXCHANGE_TYPE = 'DIRECT';
+    public const DEFAULT_EXCHANGE = null;
 
     /**
      * An associative array witch defines the
